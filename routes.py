3133beb489ad5f76bc9c2ca67d4a9a95f363af18
Thulio <thuliofmcarvalho@gmail.com>
2024-06-06 22:02:38 -0300

    Aplicado melhorias na validação dos dados de entrada,tratamento de exceções com try-except e mensagens de erros claras para uma melhor depuração do erro

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, jsonify
 from service import Service
 from app import app
 
@@ -7,8 +7,23 @@ service = Service()
 
 @app.route('/profileUrl', methods=['POST'])
 def profile_url():
-    data = request.get_json()
-    data_str = data['data']
-    profile_data = service.get_profile(data_str)
-    return profile_data, 200
+    try:
+        # Verifica se o JSON recebido contém a chave 'data'
+        data = request.get_json()
+        if not data or 'data' not in data:
+            return jsonify({'error': 'Invalid input, "data" key is missing'}), 400
 
+        data_str = data['data']
+
+        # chamar o serviço para obter os dados do perfil
+        profile_data = service.get_profile(data_str)
+
+        # Verifique se o profile_data contém um erro
+        if 'erro' in profile_data:
+            return jsonify({'error': profile_data['erro']}), 500
+
+        return jsonify(profile_data), 200
+    except Exception as e:
+        # Log do erro
+        app.logger.error(f"Erro ao processar a solicitação: {e}")
+        return jsonify({'error': 'Internal server error'}), 500
